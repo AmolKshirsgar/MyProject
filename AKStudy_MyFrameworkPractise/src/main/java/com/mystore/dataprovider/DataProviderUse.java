91f889026eeec7506151aaf11e765e4dce690662
AmolKshirsagar <ap.kshirsagar01@gmail.com>
2022-09-16 18:09:39 +0530

    Updated Changes

--- AKStudy_MyFrameworkPractise/src/main/java/com/mystore/dataprovider/DataProviderUse.java
+++ AKStudy_MyFrameworkPractise/src/main/java/com/mystore/dataprovider/DataProviderUse.java
@@ -11,14 +11,24 @@ import com.mystore.utility.DataProvider_jsonReader;
 
 public class DataProviderUse extends DataProvider_jsonReader 
 {
-	@DataProvider   //use for login
-		public Object[][] CredentialsLogin() throws IOException
-		{
-			//create json file use below method i.e getJsonDataMap and provide path of file in below argument and execute as per requiremet
-			 List<HashMap<String, String>>data=getJsonDataMap(System.getProperty("user.dir")+"\\TestData\\CredentialsLogin.json");
-			 return new Object[][] {{data.get(0)},{data.get(1)}};
-		}
+	@DataProvider   //use for login for 2 users
+//		public Object[][] CredentialsLogin() throws IOException
+//		{
+//			//create json file use below method i.e getJsonDataMap and provide path of file in below argument and execute as per requiremet
+//			 List<HashMap<String, String>>data=getJsonDataMap(System.getProperty("user.dir")+"\\TestData\\CredentialsLogin.json");
+//			 return new Object[][] {{data.get(0)},{data.get(1)}};
+//		}
 	
+	
+	//logiin with one user
+	public Object[] CredentialsLogin() throws IOException
+	{
+		//create json file use below method i.e getJsonDataMap and provide path of file in below argument and execute as per requiremet
+		 List<HashMap<String, String>>data=getJsonDataMap(System.getProperty("user.dir")+"\\TestData\\CredentialsLogin.json");
+		 return new Object[] {data.get(0)};
+	}
+
+
 	@DataProvider  //use for createaccount page
 	public Object[] CredentialsCreateAccount() throws IOException
 	{
